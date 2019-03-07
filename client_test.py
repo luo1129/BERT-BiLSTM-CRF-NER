ef3099482f1969985368e3d1f330f39196b84a10
罗宇 <luo1129@gmail.com>
2019-03-07 09:48:30 +0800

    啊啊啊

--- client_test.py
+++ client_test.py
@@ -6,14 +6,15 @@
  @Author  : MaCan (ma_cancan@163.com)
  @File    : client_test.py
 """
+import sys
 import time
 from bert_base.client import BertClient
 
 
-def ner_test():
+def ner_test(str):
     with BertClient(show_server_config=False, check_version=False, check_length=False, mode='NER') as bc:
         start_t = time.perf_counter()
-        str = '1月24日，新华社对外发布了中央对雄安新区的指导意见，洋洋洒洒1.2万多字，17次提到北京，4次提到天津，信息量很大，其实也回答了人们关心的很多问题。'
+        # str = '1月24日，新华社对外发布了中央对雄安新区的指导意见，洋洋洒洒1.2万多字，17次提到北京，4次提到天津，信息量很大，其实也回答了人们关心的很多问题。'
         rst = bc.encode([str, str])
         print('rst:', rst)
         print(time.perf_counter() - start_t)
@@ -32,4 +33,4 @@ def class_test():
 
 if __name__ == '__main__':
     # class_test()
-    ner_test()
+    ner_test(sys.arg[1])
